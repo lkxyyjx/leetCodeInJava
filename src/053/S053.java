5b6d35d408a18d087b6d8a68b6381b461a70378a
lkxyyjx <lkxyyjx@gmail.com>
2019-06-24 22:48:42 +0800

    把自己绕没了 人没了 难受

--- src/053/S053.java
+++ src/053/S053.java
@@ -1,12 +1,11 @@
 public class S053 {
     public static void main(String[] args) {
         S053 solution = new S053();
-        System.out.println(solution.maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
     public int maxSubArray(int[] nums) {
-        //resArr[0]=>pLeft, [1]=>pRight, [2]=>maxSubArraySum
-        int[] resArr;
+        int res;
         int len = nums.length;
         if (len < 1) {
             return 0;
@@ -14,66 +13,44 @@ public class S053 {
             return nums[0];
         }
 
-        resArr = dfs(0, len - 1, nums, "");
-
-        System.out.println(resArr[0]);
-        System.out.println(resArr[1]);
-        return resArr[2];
-    }
-
-    private int[] dfs(int left, int right, int[] nums, String part) {
-        int[] res = new int[3];
-        if (left == right) {
-            res[0] = res[1] = left;
-            res[2] = nums[left];
-            return res;
-        }
-
-        int mid = left + (right - left)/2;
-        int[] leftRes;
-        int[] rightRes;
-        leftRes = dfs(left, mid, nums, "left");
-        rightRes = dfs(mid + 1, right, nums, "right");
-
-        int midSum;
-        if (leftRes[1] < mid || rightRes[0] > mid + 1) {
-            midSum = 0;
-            for (int i = leftRes[1] + 1; i < rightRes[0]; i++ ) {
-                midSum += nums[i];
-            }
-            if (Integer.min(leftRes[2], rightRes[2]) + midSum > 0) {
-                res[0] = leftRes[0];
-                res[1] = rightRes[1];
-                res[2] = midSum + leftRes[2] + rightRes[2];
-            } else if (leftRes[2] > rightRes[2]) {
-                res = leftRes;
-            } else if (leftRes[2] < rightRes[2]) {
-                res = rightRes;
-            } else {
-                if (part == "left") {
-                    res = rightRes;
-                } else {
-                    res = leftRes;
-                }
-            }
-        } else {
-            if (leftRes[2] > 0 && rightRes[2] > 0) {
-                res[0] = leftRes[0];
-                res[1] = rightRes[1];
-                res[2] = leftRes[2] + rightRes[2];
-            } else if (leftRes[2] > rightRes[2]) {
-                res = leftRes;
-            } else if (leftRes[2] < rightRes[2]) {
-                res = rightRes;
-            } else{
-                if (part == "left") {
-                    res = rightRes;
-                } else {
-                    res = leftRes;
-                }
-            }
-        }
+        res = dfs(0, len - 1, nums);
 
         return res;
+    }
+
+    private int dfs(int left, int right, int[] nums) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int mid = left + (right - left) / 2;
+        int leftRes;
+        int rightRes;
+        int midRes;
+        leftRes = dfs(left, mid, nums);
+        rightRes = dfs(mid + 1, right, nums);
+        midRes = maxSubArrayAll(left, mid, right, nums);
+
+        return Integer.max(leftRes, Integer.max(rightRes, midRes));
+    }
+
+    private int maxSubArrayAll(int left, int mid, int right, int[] nums) {
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+            }
+        }
+        sum = 0;
+        int rightSum = Integer.MIN_VALUE;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+            }
+        }
+        return leftSum + rightSum;
     }
 }
