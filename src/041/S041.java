51ecf306dccda925a573b51485a1afb67060cd53
lkxyyjx <lkxyyjx@163.com>
2019-12-30 22:49:49 +0800

    1、更新41题最优解 2、更新ignore，添加idea编辑器信息

---
+++ src/041/S041.java
@@ -0,0 +1,43 @@
+public class S041 {
+    static boolean ifdebug = true;
+
+    public static void main(String args[]) {
+        S041 solution = new S041();
+        System.out.println(solution.firstMissingPositive(new int[]{-1,4,2,1,9,10}));
+    }
+
+    public int firstMissingPositive(int[] nums) {
+        int result = -1;
+        if (nums.length < 1) return 1;
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
+                temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+                if (ifdebug) {
+                    System.out.println(i + "round");
+                    for (int j = 0; j < nums.length; j++) {
+                        System.out.println("nums[" + j + "] = " + nums[j]);
+                    }
+                }
+                while (temp > 0 && temp < nums.length && nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
+                    temp = nums[nums[i] - 1];
+                    nums[nums[i] - 1] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                result = i + 1;
+                break;
+            }
+        }
+
+        if (result == -1) result = nums[nums.length - 1] + 1;
+
+        return result;
+    }
+}
