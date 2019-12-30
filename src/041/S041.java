39dfe63641df791607619e8d63e7b4af3fad484e
lkxyyjx <lkxyyjx@163.com>
2019-12-30 22:53:08 +0800

    删除冗余代码

--- src/041/S041.java
+++ src/041/S041.java
@@ -11,20 +11,15 @@ public class S041 {
         if (nums.length < 1) return 1;
         int temp = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
+            while (nums[i] > 0 && nums[i] < nums.length && nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
                 temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
-                if (ifdebug) {
-                    System.out.println(i + "round");
-                    for (int j = 0; j < nums.length; j++) {
-                        System.out.println("nums[" + j + "] = " + nums[j]);
-                    }
-                }
-                while (temp > 0 && temp < nums.length && nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
-                    temp = nums[nums[i] - 1];
-                    nums[nums[i] - 1] = nums[i];
-                    nums[i] = temp;
+            }
+            if (ifdebug) {
+                System.out.println(i + "round");
+                for (int j = 0; j < nums.length; j++) {
+                    System.out.println("nums[" + j + "] = " + nums[j]);
                 }
             }
         }
